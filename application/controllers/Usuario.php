069b3e9bc773efd547d4cf1b403f33b80e576838
matheusnarciso@hotmail.com <matheusnarciso@hotmail.com>
2020-03-19 07:55:02 -0300

    aaa

---
+++ application/controllers/Usuario.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Usuario extends CI_Controller {
+
+	/*
+	 * router usuario/cadastrar
+	 */
+	public function cadastrar(){
+		
+	}
+
+	/*
+	 * router usuario/logar
+	 */
+	public function logar(){
+
+	}
+
+	/*
+	 * router usuario/recuperar/senha
+	 */
+	public function recuperar(){
+
+	}
+}
